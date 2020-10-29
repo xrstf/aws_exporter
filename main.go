6c7574a24dea1ffefbc2cdc5e730b4b977d108ea
xrstf <git@xrstf.de>
2020-10-29 15:06:01 +0100

    use goroutines

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"flag"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"go.xrstf.de/aws_exporter/pkg/metrics"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/prometheus/client_golang/prometheus"
@@ -19,6 +19,7 @@ import (
 type options struct {
 	accessKeyID string
 	secretKey   string
+	regions     string
 	listenAddr  string
 	debugLog    bool
 }
@@ -30,6 +31,7 @@ func main() {
 
 	flag.StringVar(&opt.accessKeyID, "access-key-id", opt.accessKeyID, "AWS access key ID ($AWS_ACCESS_KEY_ID)")
 	flag.StringVar(&opt.secretKey, "secret-key", opt.secretKey, "AWS secret key ($AWS_SECRET_KEY)")
+	flag.StringVar(&opt.regions, "regions", opt.regions, "comma-separated list of regions to scan (if empty, all regions are scanned)")
 	flag.StringVar(&opt.listenAddr, "listen", opt.listenAddr, "address and port to listen on")
 	flag.BoolVar(&opt.debugLog, "debug", opt.debugLog, "enable more verbose logging")
 	flag.Parse()
@@ -61,12 +63,16 @@ func main() {
 		log.Fatal("No secret key given.")
 	}
 
+	regions := []string{}
+	if opt.regions != "" {
+		regions = strings.Split(opt.regions, ",")
+	}
+
 	creds := credentials.NewStaticCredentials(opt.accessKeyID, opt.secretKey, "")
-	config := aws.NewConfig().WithCredentials(creds).WithRegion("eu-central-1")
 	sess := session.Must(session.NewSession())
 
 	log.Info("Preparing metrics collector…")
-	prometheus.MustRegister(metrics.NewCollector(log, sess, config, []string{"eu-central-1"}))
+	prometheus.MustRegister(metrics.NewCollector(log, sess, creds, regions))
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Printf("Starting server on %s…", opt.listenAddr)
--- pkg/metrics/collector.go
+++ pkg/metrics/collector.go
@@ -1,7 +1,10 @@
 package metrics
 
 import (
+	"sync"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/prometheus/client_golang/prometheus"
@@ -11,15 +14,15 @@ import (
 type Collector struct {
 	log     *logrus.Logger
 	sess    *session.Session
-	config  *aws.Config
+	creds   *credentials.Credentials
 	regions []string
 }
 
-func NewCollector(log *logrus.Logger, sess *session.Session, config *aws.Config, regions []string) *Collector {
+func NewCollector(log *logrus.Logger, sess *session.Session, creds *credentials.Credentials, regions []string) *Collector {
 	return &Collector{
 		log:     log,
 		sess:    sess,
-		config:  config,
+		creds:   creds,
 		regions: regions,
 	}
 }
@@ -41,7 +44,7 @@ func (mc *Collector) Collect(ch chan<- prometheus.Metric) {
 func (mc *Collector) collect(ch chan<- prometheus.Metric) error {
 	regions := mc.regions
 
-	creds, err := mc.config.Credentials.Get()
+	creds, err := mc.creds.Get()
 	if err != nil {
 		return err
 	}
@@ -59,39 +62,58 @@ func (mc *Collector) collect(ch chan<- prometheus.Metric) error {
 		}
 	}
 
+	wg := sync.WaitGroup{}
+
 	for _, region := range regions {
-		log := mc.log.WithField("region", region)
+		wg.Add(1)
 
-		ch <- constMetric(regionInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region)
+		go func(log logrus.FieldLogger, region string) {
+			regionLog := log.WithField("region", region)
+			regionLog.Debug("Scanning region…")
 
-		log.Debug("Listing VPCs…")
-		vpcs, err := mc.listVPCs(region)
-		if err != nil {
-			return err
-		}
-
-		log.Debugf("Found %d VPCs.", len(vpcs))
-		for _, vpc := range vpcs {
-			ch <- constMetric(vpcInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *vpc.VpcId)
-
-			for _, tag := range vpc.Tags {
-				ch <- constMetric(vpcTag, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *vpc.VpcId, *tag.Key, *tag.Value)
+			if err := mc.collectRegion(regionLog, creds, ch, region); err != nil {
+				regionLog.Errorf("Failed to scan: %v", err)
 			}
+
+			wg.Done()
+		}(mc.log, region)
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func (mc *Collector) collectRegion(log logrus.FieldLogger, creds credentials.Value, ch chan<- prometheus.Metric, region string) error {
+	ch <- constMetric(regionInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region)
+
+	log.Debug("Listing VPCs…")
+	vpcs, err := mc.listVPCs(region)
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Found %d VPCs.", len(vpcs))
+	for _, vpc := range vpcs {
+		ch <- constMetric(vpcInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *vpc.VpcId)
+
+		for _, tag := range vpc.Tags {
+			ch <- constMetric(vpcTag, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *vpc.VpcId, *tag.Key, *tag.Value)
 		}
+	}
 
-		log.Debug("Listing subnets…")
-		subnets, err := mc.listSubnets(region)
-		if err != nil {
-			return err
-		}
+	log.Debug("Listing subnets…")
+	subnets, err := mc.listSubnets(region)
+	if err != nil {
+		return err
+	}
 
-		log.Debugf("Found %d subnets.", len(subnets))
-		for _, subnet := range subnets {
-			ch <- constMetric(subnetInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *subnet.VpcId, *subnet.SubnetId)
+	log.Debugf("Found %d subnets.", len(subnets))
+	for _, subnet := range subnets {
+		ch <- constMetric(subnetInfo, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *subnet.VpcId, *subnet.SubnetId)
 
-			for _, tag := range subnet.Tags {
-				ch <- constMetric(subnetTag, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *subnet.VpcId, *subnet.SubnetId, *tag.Key, *tag.Value)
-			}
+		for _, tag := range subnet.Tags {
+			ch <- constMetric(subnetTag, prometheus.GaugeValue, 1.0, creds.AccessKeyID, region, *subnet.VpcId, *subnet.SubnetId, *tag.Key, *tag.Value)
 		}
 	}
 
@@ -104,7 +126,8 @@ func constMetric(desc *prometheus.Desc, valueType prometheus.ValueType, value fl
 }
 
 func (mc *Collector) listRegions() ([]*ec2.Region, error) {
-	ec2Service := ec2.New(mc.sess, mc.config)
+	// must use a dummy region even to list regions
+	ec2Service := ec2.New(mc.sess, mc.awsConfig("us-west-1"))
 
 	output, err := ec2Service.DescribeRegions(nil)
 	if err != nil {
@@ -115,7 +138,7 @@ func (mc *Collector) listRegions() ([]*ec2.Region, error) {
 }
 
 func (mc *Collector) listVPCs(region string) ([]*ec2.Vpc, error) {
-	ec2Service := ec2.New(mc.sess, mc.config.WithRegion(region))
+	ec2Service := ec2.New(mc.sess, mc.awsConfig(region))
 	perPage := int64(50)
 	result := []*ec2.Vpc{}
 
@@ -142,7 +165,7 @@ func (mc *Collector) listVPCs(region string) ([]*ec2.Vpc, error) {
 }
 
 func (mc *Collector) listSubnets(region string) ([]*ec2.Subnet, error) {
-	ec2Service := ec2.New(mc.sess, mc.config.WithRegion(region))
+	ec2Service := ec2.New(mc.sess, mc.awsConfig(region))
 	perPage := int64(50)
 	result := []*ec2.Subnet{}
 
@@ -166,4 +189,13 @@ func (mc *Collector) listSubnets(region string) ([]*ec2.Subnet, error) {
 	}
 
 	return result, nil
+}
+
+func (mc *Collector) awsConfig(region string) *aws.Config {
+	config := aws.NewConfig().WithCredentials(mc.creds)
+	if region != "" {
+		config = config.WithRegion(region)
+	}
+
+	return config
 }
